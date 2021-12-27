a01c83a986fa535e43bf9b58f473826adfd5bec9
SEugene <sea.05@list.ru>
2021-12-28 00:16:27 +0300

    Lesson 1. Superuser created. Code fixed

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,7 +1,8 @@
-from authors.views import AuthorModelViewSet
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
+from authors.views import AuthorModelViewSet
 from users.views import UserModelViewSet
 
 router = DefaultRouter()
